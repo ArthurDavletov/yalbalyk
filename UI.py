545e7077fea44a3ac6883232c7d95183b0c5e736
George050 <IStormer8@gmail.com>
2024-12-14 01:02:13 +0500

    Окно рыбалки + булщит в мастер

--- UI.py
+++ UI.py
@@ -2,8 +2,6 @@ import pygame
 import main
 
 
-#butt = pygame.image.load("butt_pic.png")
-#butt_pres = pygame.image.load("butt_clicked.png")
 # ВСЕ, ЧТО ЗАКОММЕНЧЕНО, НУЖНО НА СЛУЧАЙ, ЕСЛИ КНОПКУ НАДО БУДЕТ ОТРИСОВЫВАТЬ
 
 
@@ -14,28 +12,35 @@ def print_text(screen, message, x, y, font_size, font_color=(0, 0, 0), font_type
 
 
 class Button:
-    def __init__(self, width, height, x, y, message='', x_m=0, y_m=0, font=20, font_color=(0, 0, 0)):
+    def __init__(self, width, height, x, y, butt=0, message='', x_m=0, y_m=0, font=20, font_color=(0, 0, 0)):
         self.font_color = font_color
         self.width = width
         self.height = height
-        #self.act_cl = butt
-        #self.inact_cl = butt_pres
         self.message = message
         self.x = x
         self.y = y
         self.x_m = x_m
         self.y_m = y_m
         self.font = font
+        self.flag = False
+        if butt != 0:
+            self.button = pygame.transform.scale(butt, (self.width, self.height))
 
     def draw_start(self, screen):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
         if self.x <= int(mouse[0]) <= int(self.x + self.width) and self.y < int(mouse[1]) < self.y + self.height:
-            #cache = pygame.transform.scale(butt_pres, (self.width, self.height))
-            #screen.blit(cache, (self.x, self.y))
             if click[0] == 1:
                 main.start_game(screen)
-        #else:
-            #cache = pygame.transform.scale(butt, (self.width, self.height))
-            #screen.blit(cache, (self.x, self.y))
-        #print_text(screen, self.message, self.x_m, self.y_m, self.font, self.font_color)
+
+    def draw_fishing(self, screen):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        screen.blit(self.button, (self.x, self.y))
+        if self.x <= int(mouse[0]) <= int(self.x + self.width) and self.y < int(mouse[1]) < self.y + self.height:
+            if click[0] == 1 and self.flag:
+                self.flag = False
+                return True
+        else:
+            self.flag = True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import UI
 import random
+import time
 
 WIDTH = 863
 HEIGHT = 503
@@ -13,24 +14,45 @@ def start_menu(screen):
     game_running = True
     start_butt = UI.Button(210, 70, 30, 400)
     while game_running:
+        screen.blit(bg_menu, (0, 0))
+        start_butt.draw_start(screen)
         for event in pygame.event.get():
-            screen.blit(bg_menu, (0, 0))
-            start_butt.draw_start(screen)
             if event.type == pygame.QUIT:
                 game_running = False
-            pygame.display.update()
+        pygame.display.update()
     return
 
 
 def start_game(screen):
     game_running = True
+    is_fishing = False
+    fish_founded = False
+    fishing_butt = UI.Button(200, 70, 240, 380, butt=pygame.image.load("Source/Button.png"))
+    infishing_butt = UI.Button(200, 70, 240, 380, butt=pygame.image.load("Source/Stop Button.png"))
+    timer = 0
+    last_time = 0
+    fishing_window = pygame.image.load("Source/Fishing Window.png")
     while game_running:
+        screen.blit(bg_ingame, (0, 0))
+        if not is_fishing:
+            fish_founded = False
+            is_fishing = fishing_butt.draw_fishing(screen)
+            if is_fishing:
+                timer = time.time()
+        else:
+            #гэмблинг на поимку с шансом, увеличивающимся каждую секунду
+            if not fish_founded and random.randint(0, 100) > 100 - (time.time() - timer) and int(last_time) < int(time.time()):
+                fish_founded = True
+            if fish_founded:
+                screen.blit(fishing_window, (250, 50))
+            is_fishing = not infishing_butt.draw_fishing(screen)
+            last_time = time.time()
         for event in pygame.event.get():
-            screen.blit(bg_ingame, (0, 0))
             if event.type == pygame.QUIT:
                 game_running = False
-            pygame.display.update()
+        pygame.display.update()
     return
+
 
 if __name__ == '__main__':
     pygame.init()
